98d661aa22cf55a1ca2cdedb6e6c6184743f41de
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-04-08 20:00:36 -0500

    updating

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dev_db_url = 'mongodb+srv://srisudheera:sudheera@6272@cluster0-1yttw.mongodb.net/LocalLibrary?retryWrites=true&w=majority'
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
