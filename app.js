2c77947c59d1e54b4f9d5bbd675efd6015b159b7
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-04-16 14:16:31 -0500

    completed module 6

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config({ path: '.env' })
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const dev_db_url = 'mongodb+srv://srisudheera:mypw@cluster0-1yttw.mongodb.net/LocalLibrary?retryWrites=true&w=majority'
+const dev_db_url =  process.env.ATLAS_URI
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
